fe5632d78fe99ba0b8538f328b1b14c9d3d0f636
ayushgoswami12 <ayushgoswami2005ag@gmail.com>
2026-06-20 23:19:22 +0530

    update

--- embeddingModels/chai.py
+++ embeddingModels/chai.py
@@ -1,4 +1,19 @@
 from dotenv import load_dotenv
+from pydantic import BaseModel 
+from typing import List , Optional 
+
+#This is schema 
+class Movie(BaseModel) :
+    title : str 
+    release_year : Optional[int]
+    genre : List[str]
+    director : Optional[str]
+    cast : List[str]
+    rating : Optional[float]
+    summary :  str 
+     
+    
+
 
 load_dotenv()
 
@@ -6,7 +21,9 @@ load_dotenv()
 from langchain_groq import ChatGroq
 
 model = ChatGroq(model="groq/compound-mini")
+#added to git 
 # temperature 0 to 1 , 0 => facts  , 1 => creativity 
 
 response = model.invoke("Analyse good stocks for today and give me 3 stock names ? ") 
 print(response.content)
+#made some chagnes  
